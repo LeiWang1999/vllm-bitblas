781b050a409208f4863736464e910f0742e59997
Roger Wang <136131678+ywang96@users.noreply.github.com>
2024-07-21 17:37:51 -0700

    [Model] Initial Support for Chameleon (#5770)
    Signed-off-by: LeiWang1999 <leiwang1999@outlook.com>

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -16,7 +16,9 @@ _GENERATION_MODELS = {
     "BaiChuanForCausalLM": ("baichuan", "BaiChuanForCausalLM"),  # baichuan-7b
     "BaichuanForCausalLM": ("baichuan", "BaichuanForCausalLM"),  # baichuan-13b
     "BloomForCausalLM": ("bloom", "BloomForCausalLM"),
-    "BitnetForCausalLM": ("bitnet", "BitnetForCausalLM"),
+    "ChameleonForCausalLM":
+    ("chameleon", "ChameleonForConditionalGeneration"
+     ),  #TODO(ywang96): fix model name when huggingface fixes it
     "ChatGLMModel": ("chatglm", "ChatGLMForCausalLM"),
     "ChatGLMForConditionalGeneration": ("chatglm", "ChatGLMForCausalLM"),
     "CohereForCausalLM": ("commandr", "CohereForCausalLM"),
