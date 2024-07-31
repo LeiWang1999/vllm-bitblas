bf544891712a04ea5f65969a4b5b6b948d5486ff
Cyrus Leung <tlleungac@connect.ust.hk>
2024-07-31 10:38:03 +0800

    [mypy] Enable following imports for some directories (#6681)
    Signed-off-by: LeiWang1999 <leiwang1999@outlook.com>

--- vllm/config.py
+++ vllm/config.py
@@ -773,7 +773,7 @@ class ParallelConfig:
                         backend)
 
         self._verify_args()
-        self.rank = 0
+        self.rank: int = 0
 
     @property
     def use_ray(self) -> bool:
@@ -902,6 +902,7 @@ class SchedulerConfig:
 
 
 class DeviceConfig:
+    device: Optional[torch.device]
 
     def __init__(self, device: str = "auto") -> None:
         if device == "auto":
