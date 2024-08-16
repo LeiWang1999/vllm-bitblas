ca501bc9d33c6e4f1d8733547e6f20392b69c610
Mor Zusman <mor.zusmann@gmail.com>
2024-08-16 20:06:51 +0300

    [Kernel] W8A16 Int8 inside FusedMoE  (#7415)
    Signed-off-by: LeiWang1999 <leiwang1999@outlook.com>

--- vllm/config.py
+++ vllm/config.py
@@ -249,8 +249,9 @@ class ModelConfig:
         supported_quantization = [*QUANTIZATION_METHODS]
         rocm_supported_quantization = ["gptq", "squeezellm", "fp8"]
         optimized_quantization_methods = [
-            "fp8", "marlin", "gptq_marlin_24", "gptq_marlin", "gptq_bitblas",
-            "bitblas", "bitnet_bitblas", "compressed_tensors"
+            "fp8", "marlin", "gptq_marlin_24", "gptq_marlin", "awq_marlin",
+            "fbgemm_fp8", "compressed_tensors", "compressed-tensors",
+            "experts_int8"
         ]
         tpu_supported_quantization = ["tpu_int8"]
         if self.quantization is not None:
